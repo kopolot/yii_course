45daf4ef6d93bf6a3b4e2104a4b786452f319f37
kopolot <konradmajkrzyk@gmail.com>
2022-04-14 21:37:49 +0200

    sdflkas

---
+++ models/Article.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+use app\models\User;
+
+/**
+ * This is the model class for table "article".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $slug
+ * @property string $text
+ * @property int|null $created_by
+ * @property int|null $created_at
+ * @property int|null $updated_at
+ *
+ * @property Users $createdBy
+ */
+class Article extends ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'article';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'slug', 'text'], 'required'],
+            [['text'], 'string'],
+            [['created_by', 'created_at', 'updated_at'], 'integer'],
+            [['title', 'slug'], 'string', 'max' => 55],
+            [['title'], 'unique'],
+            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['created_by' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'slug' => 'Slug',
+            'text' => 'Text',
+            'created_by' => 'Created By',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(User::class, ['id' => 'created_by']);
+    }
+}
